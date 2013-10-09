ee6535d70c1e340c9cb8ab7063b0d1607c210abf
Rahul <techniciablog@gmail.com>
2013-10-09 19:25:23 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Broomble::Application.config.secret_token = 'e9f0bfe012624c401e77996a70bb55e8a22b7d68468782224ff675ff4e97d025418d27e593cec37495a64ceb5d812491c8b49fead4fed0b48bf80ff2dce19363'
