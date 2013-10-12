807befaa7a9c5e7e15bd09b505df131a5d104004
Rahul <techniciablog@gmail.com>
2013-10-12 12:58:07 +0530

    Display submitted link

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,15 @@
 class LinksController < ApplicationController
+	
+	respond_to :html, :xml, :js
+
+	def index
+		@links = Link.all
+	end
+
 	def show
+		@link = Link.find(params[:id])
+
+		respond_with @link
 	end
 
 	def new
@@ -7,5 +17,10 @@ class LinksController < ApplicationController
 	end
 
 	def create
+		@link = Link.new(params[:id])
+		@link.save
+
+		flash.notice = "Link has been submitted"
+		redirect_to link_path(@link)
 	end
 end
